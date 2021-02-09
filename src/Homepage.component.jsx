cdc2e4d7af389f6881f5f2fd82d8af32051b0617
Srinath Hakeem <srinathhakeem@srinaths-mbp.myfiosgateway.com>
2021-02-09 14:35:20 -0500

    created files and added files for homepage

---
+++ src/Homepage.component.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import './Homepage.styles.scss'
+
+const Homepage = () => {
+ return (
+  <div classname='homepage'>
+   <div className='directory-menu'>
+    <div className='menu-item'>
+     <div className='content'>
+      <h1 className='title'>HATS</h1>
+      <span className='sub-title'>SHOP Now</span>
+     </div>
+    </div>
+     <div className='menu-item'>
+     <div className='content'>
+      <h1 className='title'>JACKETS</h1>
+      <span className='sub-title'>SHOP Now</span>
+     </div>
+    </div>
+     <div className='menu-item'>
+     <div className='content'>
+      <h1 className='title'>SNEAKERS</h1>
+      <span className='sub-title'>SHOP Now</span>
+     </div>
+    </div>
+     <div className='menu-item'>
+     <div className='content'>
+      <h1 className='title'>WOMENS</h1>
+      <span className='sub-title'>SHOP Now</span>
+     </div>
+    </div>
+     <div className='menu-item'>
+     <div className='content'>
+      <h1 className='title'>MENS</h1>
+      <span className='sub-title'>SHOP Now</span>
+     </div>
+    </div>
+     <div className='menu-item'>
+     <div className='content'>
+      <h1 className='title'>HOME</h1>
+      <span className='sub-title'>SHOP Now</span>
+     </div>
+    </div>
+   </div>
+  </div>
+ )
+}
+
+export default Homepage;
