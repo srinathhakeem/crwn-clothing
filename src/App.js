344c6fee386b0949f29909b8d15a28b0a82abda4
Srinath Hakeem <srinathhakeem@srinaths-mbp.myfiosgateway.com>
2021-02-09 23:11:35 -0500

    created files and added the components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Homepage from './Homepage.component';
+import Homepage from './pages/homepage/homepage.component';
 
 function App() {
   return (
